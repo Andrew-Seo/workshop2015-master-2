7a00ccb9c1dd0dc144dd5ba93ca66ee0ba6efee1
League Mac <league@WTS15.attlocal.net>
2016-04-29 18:29:56 -0700

    progress 2

--- IntroToJava/src/Level_2/Atari_Breakout.java
+++ IntroToJava/src/Level_2/Atari_Breakout.java
@@ -23,7 +23,7 @@ public class Atari_Breakout implements KeyListener, MouseMotionListener
 
 	public final static int panelWidth = 1900;
 	public final static int panelHeight = 1000;
-	
+
 	public static void main(String[] args)
 	{
 
@@ -49,48 +49,49 @@ public class Atari_Breakout implements KeyListener, MouseMotionListener
 		frame.add(panel);
 
 		frame.setSize(panelWidth, panelHeight);
-//
-//		int currentX = 0;
-//		int currentY = 400;
-//
-//		// Create Blox
-//		Blocks.Blox[0] = new Blocks(0, 0, 2400, 5, false, Color.BLUE);
-//		Blocks.Blox[1] = new Blocks(1895, 0, 5, 1000, false, Color.BLUE);
-//		Blocks.Blox[2] = new Blocks(0, 0, 5, 1000, false, Color.BLUE);
-//
-//		/*
-//		 * for (int i = 0; i < 1; i++) { Random random = new Random(); int
-//		 * rWidth = random.nextInt(70) + 60;
-//		 * 
-//		 * Blocks.Blox[i + 3] = new Blocks(currentX, currentY, 1000, 50, true,
-//		 * new Color((int) (Math.random() * 255), (int)
-//		 * 
-//		 * (Math.random() * 255), (int) (Math.random() * 255))); currentX =
-//		 * currentX + rWidth + 5; if (currentX > 1850) { currentY = currentY +
-//		 * 60; if (1 - i < 12) { break; } currentX = 0; } }
-//		 * 
-//		 * }
-//		 */
-//
-//		for (int i = 0; i < 76; i++)
-//		{
-//			Random random = new Random();
-//			int rWidth = random.nextInt(70) + 60;
-//
-//			Blocks.Blox[i + 3] = new Blocks(currentX, currentY, rWidth, 50, true, new Color((int) (Math.random() * 255), (int)
-//
-//			(Math.random() * 255), (int) (Math.random() * 255)));
-//			currentX = currentX + rWidth + 5;
-//			if (currentX > 1850)
-//			{
-//				currentY = currentY + 60;
-//				if (76 - i < 12)
-//				{
-//					break;
-//				}
-//				currentX = 0;
-//			}
-//		}
+		//
+		// int currentX = 0;
+		// int currentY = 400;
+		//
+		// // Create Blox
+		// Blocks.Blox[0] = new Blocks(0, 0, 2400, 5, false, Color.BLUE);
+		// Blocks.Blox[1] = new Blocks(1895, 0, 5, 1000, false, Color.BLUE);
+		// Blocks.Blox[2] = new Blocks(0, 0, 5, 1000, false, Color.BLUE);
+		//
+		// /*
+		// * for (int i = 0; i < 1; i++) { Random random = new Random(); int
+		// * rWidth = random.nextInt(70) + 60;
+		// *
+		// * Blocks.Blox[i + 3] = new Blocks(currentX, currentY, 1000, 50, true,
+		// * new Color((int) (Math.random() * 255), (int)
+		// *
+		// * (Math.random() * 255), (int) (Math.random() * 255))); currentX =
+		// * currentX + rWidth + 5; if (currentX > 1850) { currentY = currentY +
+		// * 60; if (1 - i < 12) { break; } currentX = 0; } }
+		// *
+		// * }
+		// */
+		//
+		// for (int i = 0; i < 76; i++)
+		// {
+		// Random random = new Random();
+		// int rWidth = random.nextInt(70) + 60;
+		//
+		// Blocks.Blox[i + 3] = new Blocks(currentX, currentY, rWidth, 50, true,
+		// new Color((int) (Math.random() * 255), (int)
+		//
+		// (Math.random() * 255), (int) (Math.random() * 255)));
+		// currentX = currentX + rWidth + 5;
+		// if (currentX > 1850)
+		// {
+		// currentY = currentY + 60;
+		// if (76 - i < 12)
+		// {
+		// break;
+		// }
+		// currentX = 0;
+		// }
+		// }
 
 	}
 
@@ -111,7 +112,21 @@ public class Atari_Breakout implements KeyListener, MouseMotionListener
 	public void keyPressed(KeyEvent e)
 	{
 		// TODO Auto-generated method stub
-
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			panel.changeState(panel.gameState);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			panel.changeState(panel.menu2State);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			if (panel.currentState == panel.gameWinState || panel.currentState == panel.gameLoseState)
+			{
+				panel.gameReset();
+			}
+		}
 	}
 
 	@Override
@@ -132,7 +147,7 @@ public class Atari_Breakout implements KeyListener, MouseMotionListener
 	public void mouseMoved(MouseEvent e)
 	{
 		panel.holdposition = e.getX();
-		
+
 		// TODO Auto-generated method stub
 	}
 }
